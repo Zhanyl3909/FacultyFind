90f4fe49ec96eedbf6513336eb6dbd82d09afb73
etrella39 <ju020807@naver.com>
2024-05-27 22:56:17 +0900

    edit

--- app/src/main/java/com/example/hackathon/ProgressBarTestActivity.kt
+++ app/src/main/java/com/example/hackathon/ProgressBarTestActivity.kt
@@ -1,18 +1,27 @@
-// MainActivity.java
 package com.example.hackathon
 
 import android.os.Bundle
 import android.view.View
 import android.widget.ProgressBar
+import android.widget.RelativeLayout
 import androidx.appcompat.app.AppCompatActivity
 
 class ProgressBarTestActivity : AppCompatActivity() {
     private var mProgress: ProgressBar? = null
     private var mProgressStatus = 0
     var i: Int = 0
+
+    private lateinit var backButton: RelativeLayout // 회원가입 버튼
+    private lateinit var nextButton: RelativeLayout // 회원가입 버튼
+
+
     override fun onCreate(icicle: Bundle?) {
         super.onCreate(icicle)
         setContentView(R.layout.main_screen) //check this
+
+        backButton = findViewById(R.id.back_button)
+        nextButton = findViewById(R.id.next_button)
+
         mProgress = findViewById<View>(R.id.main_screen_progress_bar) as ProgressBar
 
         Thread {
