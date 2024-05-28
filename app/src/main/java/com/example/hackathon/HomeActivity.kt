4d6c18a910623baceb98f273eba9aadd62eab906
Zhanyl3909 <dzh.2002z@gmail.com>
2024-05-28 18:52:47 +0900

    setQuestion

--- app/src/main/java/com/example/hackathon/HomeActivity.kt
+++ app/src/main/java/com/example/hackathon/HomeActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.widget.ProgressBar
 import android.widget.RadioGroup
 import android.widget.RelativeLayout
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
@@ -19,6 +20,15 @@ class HomeActivity : AppCompatActivity() {
     private lateinit var nextButton: RelativeLayout
     private lateinit var radioGroup: RadioGroup
 
+    private lateinit var questionTextView: TextView
+
+    private var currentQuestionIndex = 0
+    private val questions = listOf(
+        "다양한 문화와 언어에 관심이 많아서 해외 여행이나 교환학생 프로그램에 참여하고 싶은 편이다.",
+        "유럽이나 미주의 역사나 문화에 대해 탐구하고 공부하는 것을 좋아한다.",
+        "외국어를 배우는 것에 열정이 있어서 언어 학습 앱이나 온라인 강의를 이용하여 새로운 언어를 익히는 것을 좋아한다."
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_screen)
@@ -27,6 +37,11 @@ class HomeActivity : AppCompatActivity() {
         nextButton = findViewById(R.id.next_button)
         radioGroup = findViewById(R.id.options)
         mProgress = findViewById(R.id.main_screen_progress_bar)
+
+        questionTextView = findViewById(R.id.questionTextView)
+
+        setQuestion()
+
 
         // Start a thread to update the progress bar
         Thread {
@@ -50,9 +65,6 @@ class HomeActivity : AppCompatActivity() {
         }
 
 
-
-
-
         nextButton.setOnClickListener {
 
             val isSelected =  radioGroup.checkedRadioButtonId
@@ -60,13 +72,18 @@ class HomeActivity : AppCompatActivity() {
                 Toast.makeText(this@HomeActivity, "답변을 선택해 주세요", Toast.LENGTH_SHORT)
                     .show()
             } else {
-                Toast.makeText(this@HomeActivity, "다음", Toast.LENGTH_SHORT)
-                    .show()
+                goToNextQuestion()
+                }
             }
         }
 
 
-
-
+    private fun setQuestion() {
+        questionTextView.text = questions[currentQuestionIndex]
     }
+    private fun goToNextQuestion() {
+        currentQuestionIndex = (currentQuestionIndex + 1) % questions.size
+        setQuestion()
+    }
+
 }
