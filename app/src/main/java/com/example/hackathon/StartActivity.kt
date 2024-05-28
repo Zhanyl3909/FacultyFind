df79bcf9f999e1986f5255c1e4d24f5e0d870247
Zhanyl3909 <dzh.2002z@gmail.com>
2024-05-29 04:36:51 +0900

    add

---
+++ app/src/main/java/com/example/hackathon/StartActivity.kt
@@ -0,0 +1,39 @@
+package com.example.hackathon
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.RadioGroup
+import android.widget.RelativeLayout
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class StartActivity : AppCompatActivity() {
+
+
+    private lateinit var start_button: RelativeLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.start_screen)
+
+
+        start_button = findViewById(R.id.start_button)
+
+
+    }
+    fun onClickStart(view: View) {
+        val intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
+    }
+    start_button.setOnClickListener {
+        
+    }
+
+
+}
+
